28e1f72ceea2ba5ee3fd46c34f68e0d31c8956ff
Shivam Grover <35850682+shivam-grover@users.noreply.github.com>
2021-05-23 04:20:44 +0530

    Added comments and cleaned API code

--- RestfulAPI/script_jsonparse.rb
+++ RestfulAPI/script_jsonparse.rb
@@ -3,68 +3,55 @@ require 'uri'
 require 'json'
 require 'fileutils'
 
-# files = Dir["LinkedInData/*"]
 
+#the url on our server where I will receive the database
 url = 'http://localhost:3000/profiles'
 uri = URI.parse(url)
 
+#an array to keep track of the data records already uploaded
 data = []
 
-
+#infinite loop
 while(1)
+
+  #gets all JSON files in the LinkedInData folder. These files are those which are newly added
   files = Dir["LinkedInData/*.json"]
+
+  #gets all JSON files in the LinkedInData/Done folder. These files are those which have already been checked and uploaded if needed
   data = Dir["LinkedInData/Done/*.json"]
 
-#   # puts files.length
-#   # puts data.length
-#
-#   if(files.length > data.length)
-#     # puts "in if"
+  #check for all newly added files
     for i in 0..files.length() - 1
-#
+
+      #if they were not already uploaded only then upload them
       if(not data.include? "LinkedInData/Done/" + files[i].split("/")[1])
         data.append(files[i])
-#         # puts files[i]
+
+        #get the file
         file = File.read(files[i])
+        #parse the json file
         data_hash = JSON.parse(file)
+
+        #printing for debugging
         puts data_hash[0]['FirstName']
         puts data_hash[0]['LastName']
         puts data_hash[0]['Companies'][0]
+
+        #create object to send. I only send the first company name instead of the whole list
         params = {'firstname' => data_hash[0]['FirstName'], 'lastname' => data_hash[0]['LastName'], 'company' => data_hash[0]['Companies'][0]}
+
+        #send to server
         Net::HTTP.post_form(uri, params)
+
+        #move the upload json file to the Done directory
         FileUtils.mv(files[i], "LinkedInData/Done/" + files[i].split("/")[1])
       else
+        #if newly added file was already upload, move it to done directory. This is the equivalent of removing it.
         FileUtils.mv(files[i], "LinkedInData/Done/" + files[i].split("/")[1])
       end
 
     end
-#
-#   end
+
+  #wait for half second before looping
   sleep(0.5)
 end
-
-# files = Dir["LinkedInData/*.json"]
-# for i in 0..files.length() - 1
-#   puts files[i]
-# end
-
-# for i in 0..files.length() - 1
-#   puts files[i]
-#   file = File.read(files[i])
-#   data_hash = JSON.parse(file)
-#   puts data_hash[0]['FirstName']
-#   puts data_hash[0]['LastName']
-#   puts data_hash[0]['Companies'][0]
-#   params = {'firstname' => data_hash[0]['FirstName'], 'lastname' => data_hash[0]['LastName'], 'company' => data_hash[0]['Companies'][0]}
-#   Net::HTTP.post_form(uri, params)
-# end
-
-
-
-# puts files
-
-
-
-
-
-# params = {'firstname' => data_hash[0]['FirstName'], 'lastname' => data_hash[0]['LastName'], 'company' => data_hash[0]['Companies'][0]}
