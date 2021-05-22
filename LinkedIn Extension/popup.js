3a1c38964d0934aa970ac82b18d32cca948b8d06
Shivam Grover <35850682+shivam-grover@users.noreply.github.com>
2021-05-23 04:19:14 +0530

    Added comments and cleaned code in extension

--- LinkedIn Extension/popup.js
+++ LinkedIn Extension/popup.js
@@ -1,7 +1,9 @@
+
+// button element
 let getData = document.getElementById("getData");
 
 
-
+//adding click listener and calling getDataFromLinkedIn whenever the button in the extension is clicked
 getData.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
@@ -12,9 +14,10 @@ getData.addEventListener("click", async () => {
 });
 
 
+//the function responsible for the scraping
 function getDataFromLinkedin() {
 	
-
+	//this function is used to save the data as a JSON file
 	(function(console){
 
 	console.save = function(data, filename){
@@ -42,24 +45,31 @@ function getDataFromLinkedin() {
 	 }
 	})(console)
 
+
+	//empty object for the data
 	var profileDetails = [{}];
 
+	//getting the name from the html
 	name = document.querySelector("div.pv-text-details__left-panel").innerText.split("\n")[0];
+	
+	//splitting name into the first and the second name. If there are more than two names, we will get upto the second name. Alternatively instead of name.split(" ")[1], I can also fetch the last element to get the last name
 	profileDetails[0]['FirstName'] = name.split(" ")[0];
 	profileDetails[0]['LastName'] = name.split(" ")[1];
 
+	//initializing an empty array for storing the list of companies
 	profileDetails[0]['Companies'] = []
 
+	//getting the whole section in which companies are written and then extracting the text and splitting the whole list into different elements
 	companies = document.querySelector("#experience-section ul").innerText.split("\n\nCompany Name\n\n").slice(1);
 
 	for (i = 0; i< companies.length; i ++){
 
+		//extracting only the company name from all the data
 	    profileDetails[0]['Companies'].push(companies[i].split("\n")[0])
-	//     console.log(companies[i].split("\n")[0])
 	}
 
+	//printing the final data
 	console.log(profileDetails)
-
+	//saving the final data
 	console.save(profileDetails, profileDetails[0]["FirstName"] + " " + profileDetails[0]["LastName"] + ".json")
 }
-
